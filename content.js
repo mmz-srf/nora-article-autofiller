9c450f5b0fcaac1846aa9547f709cb6df0bbd59b
Benjamin Knecht <benjamin.knecht@srf.ch>
2024-01-03 16:59:40 +0100

    fill by cmd+b

--- content.js
+++ content.js
@@ -1,27 +1,37 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === 'fill') {
-    console.log('Autofilling...');
-
-    // TODO: Fill that damm select element!!!
-    const kicker = document.getElementById('kicker');
-    const title = document.getElementById('title');
-    const shortLead = document.getElementById('shortLead');
-    const author = document.getElementById('author');
-
-    const authors = ['Benjamin Knecht', 'Phillip Christen', 'Urban Etter', 'Pascal von Büren', 'Hasan Kryeziu'];
-
-    kicker.value = 'Das ist ein Kicker';
-    title.value = 'Das ist ein Titel';
-    shortLead.value = 'Das ist ein Shortlead';
-    author.value = authors[getRandomInt(authors.length)];
-
-    title.dispatchEvent(new Event('change', { bubbles: true }));
-    kicker.dispatchEvent(new Event('change', { bubbles: true }));
-    shortLead.dispatchEvent(new Event('change', { bubbles: true }));
-    author.dispatchEvent(new Event('change', { bubbles: true }));
+    fill();
   }
 });
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+
+function fill() {
+  console.log('Autofilling...');
+
+  // TODO: Fill that damm select element!!!
+  const kicker = document.getElementById('kicker');
+  const title = document.getElementById('title');
+  const shortLead = document.getElementById('shortLead');
+  const author = document.getElementById('author');
+
+  const authors = ['Benjamin Knecht', 'Phillip Christen', 'Urban Etter', 'Pascal von Büren', 'Hasan Kryeziu'];
+
+  kicker.value = 'Das ist ein Kicker';
+  title.value = 'Das ist ein Titel';
+  shortLead.value = 'Das ist ein Shortlead';
+  author.value = authors[getRandomInt(authors.length)];
+
+  title.dispatchEvent(new Event('change', { bubbles: true }));
+  kicker.dispatchEvent(new Event('change', { bubbles: true }));
+  shortLead.dispatchEvent(new Event('change', { bubbles: true }));
+  author.dispatchEvent(new Event('change', { bubbles: true }));
+}
+
+document.addEventListener('keydown', function(event) {
+  if (event.metaKey && event.key === 'b') {
+    fill();
+  }
+});
